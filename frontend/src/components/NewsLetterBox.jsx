0e00642905356e93e6f7d31edeec6c8fab49b2a4
Rajender216 <216rajender@gmail.com>
2025-04-04 15:05:03 +0530

    6

--- frontend/src/components/NewsLetterBox.jsx
+++ frontend/src/components/NewsLetterBox.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 
 const NewsLetterBox = () => {
-    const onSubmitHandler = (e)=>{
-        e.preventDefault();
-
-    }
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className=" text-center">
       <p className=" text-2xl font-medium text-gray-800">
@@ -18,15 +17,23 @@ const NewsLetterBox = () => {
         cupiditate magni!
       </p>
 
-      <form onSubmit={onSubmitHandler} className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3" action="">
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3"
+        action=""
+      >
         <input
           className="w-full sm:flex-1 outline-none"
           type="email"
           placeholder="Enter your Email"
         />
         <button
-        className="bg-black text-white text-xs px-10 py-4"
-         type="submit"> SUBSCRIBE</button>
+          className="bg-black text-white text-xs px-10 py-4"
+          type="submit"
+        >
+          {" "}
+          SUBSCRIBE
+        </button>
       </form>
     </div>
   );
