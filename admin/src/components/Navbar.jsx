1ee3363eadac40fd3e26159774e6258d445d109c
Rajender216 <216rajender@gmail.com>
2025-05-09 12:13:12 +0530

    logo

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -6,7 +6,6 @@ const Navbar = ({ setToken }) => {
     <div className="flex items-center py-2 px-[4%] justify-between">
       {/* <img className='w-[max(10%,80px)]' src={assets.logo} alt="" /> */}
       <h1 className="text-2xl">HIKE N FALL</h1>
-
       <h1></h1>
       <button
         onClick={() => setToken("")}
