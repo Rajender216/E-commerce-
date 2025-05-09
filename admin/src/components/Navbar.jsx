2d0b868aae4e4f788767362286bdeeae0cdb7779
Rajender216 <216rajender@gmail.com>
2025-05-09 12:07:09 +0530

    change

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import {assets} from '../assets/assets'
+import React from "react";
+import { assets } from "../assets/assets";
 
-const Navbar = ({setToken}) => {
+const Navbar = ({ setToken }) => {
   return (
-    <div className='flex items-center py-2 px-[4%] justify-between'>
-        <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
-        <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
-    </div>
-  )
-}
+    <div className="flex items-center py-2 px-[4%] justify-between">
+      {/* <img className='w-[max(10%,80px)]' src={assets.logo} alt="" /> */}
+      <h1 className="text-2xl">HIKE N FALL</h1>
 
-export default Navbar
+      <h1></h1>
+      <button
+        onClick={() => setToken("")}
+        className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
+      >
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
